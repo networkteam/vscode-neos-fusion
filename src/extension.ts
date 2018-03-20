b727c45b84b775377b8c5970e254ff0232db871c
Christopher Hlubek <hlubek@networkteam.com>
2018-03-20 13:55:46 +0100

    Task: Get a server runnign

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,42 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import * as path from 'path';
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "neos-fusion" is now active!');
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, RevealOutputChannelOn } from 'vscode-languageclient';
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+export function activate(context: ExtensionContext) {
+    // The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('out', 'server.js'));
+	// The debug options for the server
+	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run : { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	};
 
-    context.subscriptions.push(disposable);
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for Fusion documents
+		documentSelector: [{scheme: 'file', language: 'fusion'}],
+		synchronize: {
+			// Synchronize the setting section 'neosFusion' to the server
+			configurationSection: 'neosFusion',
+			// Notify the server about file changes to '.clientrc files contain in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		},
+		revealOutputChannelOn: RevealOutputChannelOn.Info
+	};
+
+	// Create the language client and start the client.
+	let disposable = new LanguageClient('neosFusion', 'Fusion Language Server', serverOptions, clientOptions).start();
+
+	// Push the disposable to the context's subscriptions so that the
+	// client can be deactivated on extension deactivation
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
